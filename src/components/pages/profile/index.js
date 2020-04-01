a0c9d85567e13a6ec6240da0baead503b6c1726d
Cristiano <neivacristiano@yahoo.com.br>
2020-04-01 19:29:47 -0300

    end - profile

--- src/components/pages/profile/index.js
+++ src/components/pages/profile/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 import api from '../../../services/api'
 import './style.css'
@@ -10,12 +10,29 @@ class Profile extends Component{
     super(props)
     this.state = {
         incidents: [],
-        ongName: localStorage.getItem('ongName')
+        ongName: localStorage.getItem('ongName'),
+        ongId: localStorage.getItem('ongId')
     }
   }
 
+  async handleDeleteIncident(id) {
+    await api.delete(`/incidents/${id}`, {
+      headers: {authorization: this.state.ongId}
+    })
+
+    const updatedIncidents = this.state.incidents.filter(incident => incident.id !== id)
+
+    this.setState({incidents: updatedIncidents})
+  }
+
+   handleLogOut() {
+     localStorage.clear()
+
+     return this.props.history.push('/')
+   }
+  
   async componentDidMount() {
-    const { data } = await api.get('/icidents/query', {headers: {authorization: localStorage.getItem('ongId')}})
+    const { data } = await api.get('/incidents/query', {headers: {authorization: this.state.ongId}})
     this.setState({incidents: data})
   }
 
@@ -26,26 +43,26 @@ class Profile extends Component{
               <img src={logoImg} alt='Be the Hero'></img>
               <span>Bem Vinda, {this.state.ongName}</span>
               <Link className='button' to='/incidents/new'>Cadastrar novo caso</Link>
-              <button type='button'>
+              <button type='button' onClick={() => this.handleLogOut()}>
                   <FiPower size={18} color='#E02041'/>
               </button>
           </header>
             <h1>Casos Cadastrados</h1>
             <ul>
                 {
-                    this.state.incidents.map(incident => {
+                    this.state.incidents.map((incident) => {
                         return (
-                            <li>
-                                <strong>>Caso:</strong>
+                            <li key={incident.id}>
+                                <strong>Caso:</strong>
                                 <p>{incident.title}</p>
 
                                 <strong>Descrição</strong>
                                 <p>{incident.description}</p>
 
                                 <strong>Valor</strong>
-                                <p>{incident.value}</p>
+                                <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
-                                <button type='button'>
+                                <button type='button' onClick={() => this.handleDeleteIncident(incident.id)}>
                                     <FiTrash2 size={20} color='#a8a8b3'></FiTrash2>
                                 </button>
                             </li>
@@ -58,4 +75,4 @@ class Profile extends Component{
   }
 }
 
-export default Profile;
+export default withRouter(Profile);
